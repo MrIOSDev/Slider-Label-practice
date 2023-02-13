49543e7c40acef4ec826021c00a1bda8f61dc40e
Magomed <mpliev@yahoo.ru>
2023-02-13 16:47:55 +0300

    Initial Commit

---
+++ Slider-Label practice/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Slider-Label practice
+//
+//  Created by admin on 13/02/2023.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
